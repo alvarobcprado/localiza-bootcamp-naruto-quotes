7008e147fc3dc55a6c0a44f0dcd859bf3ac7960d
Álvaro Prado <alvarob.prado@hotmail.com>
2021-07-21 12:14:15 -0400

    Corrigido o export do componente Quotes

--- src/components/Quotes/index.jsx
+++ src/components/Quotes/index.jsx
@@ -1,7 +1,8 @@
-
 const Quotes = () => {
-    <div>
-      <p>quote - Speaker</p>
-      <button>Quote No Jutsu</button>
-    </div>;
-}
+  <div>
+    <p>quote - Speaker</p>
+    <button>Quote No Jutsu</button>
+  </div>;
+};
+
+export default Quotes;
