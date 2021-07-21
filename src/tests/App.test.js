47e64f0fde35a23733c6c3c96dab2415c7ae2468
Álvaro Prado <alvarob.prado@hotmail.com>
2021-07-21 12:06:55 -0400

    Corrigido o teste unitário do componente App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,5 +2,16 @@ import {
   render,
   screen,
 } from "@testing-library/react";
-
 import { App } from "../components";
+
+test("renders the app, with a button, a quote and a button", () => {
+  render(<App />);
+
+  const buttonEl = screen.getByRole("button");
+  const imageEl = screen.getByRole("img");
+  const textEl = screen.getByText(/Speaker/);
+
+  expect(buttonEl).toBeInTheDocument;
+  expect(imageEl).toBeInTheDocument;
+  expect(textEl).toBeInTheDocument;
+});
