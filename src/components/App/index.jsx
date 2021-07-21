0970c23ba6f1fdd7ca47c38ba20c60b71a7f653a
Álvaro Prado <alvarob.prado@hotmail.com>
2021-07-21 12:07:42 -0400

    Alterado o componente App para passar pelo teste

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,9 +1,19 @@
-import Home from "../../pages/home";
 // import { ThemeProvider } from "styled-components";
 // import { Reset } from "styled-reset";
+import narutoImg from "../../assets/images/naruto.png";
+import { Content, NarutoImg } from "./styles";
 
 function App() {
-  return <Home />;
+  return (
+    <Content>
+      <p>quote - Speaker</p>
+      <button>Quote No Jutsu</button>
+      <NarutoImg
+        src={narutoImg}
+        alt="Naruto with Kunai"
+      />
+    </Content>
+  );
 }
 
 export default App;
---
+++ src/components/App/styles.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+  height: 100vh;
+  padding: 0 58px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NarutoImg = styled.img`
+  max-width: 50vw;
+  align-self: flex-end;
+`;
