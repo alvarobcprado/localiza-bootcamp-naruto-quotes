576b833c6deed45129e5a8cc23a0e5024fa3a047
Álvaro Prado <alvarob.prado@hotmail.com>
2021-07-21 12:58:04 -0400

    Adicionado componente Button

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,5 @@
+const Button = ({ children, onclick }) => {
+  return <button>{children}</button>;
+};
+
+export default Button;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,2 +1,3 @@
 export { default as App } from "./App";
 export { default as Quotes } from "./Quotes";
+export { default as Button } from "./Button";
