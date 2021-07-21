01fa8e7f6d2a7aa02776c359f695f0d04dedb75c
Álvaro Prado <alvarob.prado@hotmail.com>
2021-07-21 12:28:15 -0400

    Alterado componente Quotes

--- src/components/Quotes/index.jsx
+++ src/components/Quotes/index.jsx
@@ -1,8 +1,11 @@
-const Quotes = () => {
-  <div>
-    <p>quote - Speaker</p>
-    <button>Quote No Jutsu</button>
-  </div>;
+const Quotes = ({ quote, speaker, button }) => {
+  return (
+    <div>
+      <p>{quote}</p>
+      <p>{speaker}</p>
+      <button>Quote No Jutsu</button>
+    </div>
+  );
 };
 
 export default Quotes;
