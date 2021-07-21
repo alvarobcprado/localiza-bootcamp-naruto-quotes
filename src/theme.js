3e1e87bcccf093de2d7963c97cbf470b9be8fcca
Álvaro Prado <alvarob.prado@hotmail.com>
2021-07-21 12:41:58 -0400

    Adicionado plano de fundo ao estilo global

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import bgImg from "./assets/images/bg.jpeg";
 
 export const Theme = {
   colors: {
@@ -14,9 +15,11 @@ export const Theme = {
 
 export const GlobalStyle = createGlobalStyle`
     body{
-        color: #332c36;
-        padding: 0;
-        margin: 0;
-        font-family:'New Tegomin', serif;
+      background: url(${bgImg}) center no-repeat;
+      background-size: cover;
+      color: #332c36;
+      padding: 0;
+      margin: 0;
+      font-family:'New Tegomin', serif;
     }
 `;
