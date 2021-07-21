89234b2d0fa650341343768f5f933c9f084fc799
Álvaro Prado <alvarob.prado@hotmail.com>
2021-07-21 12:28:40 -0400

    Adicionado componente Quotes ao componente App

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,10 +2,12 @@
 // import { Reset } from "styled-reset";
 import narutoImg from "../../assets/images/naruto.png";
 import { Content, NarutoImg } from "./styles";
+import { Quotes } from "../../components";
 
 function App() {
   return (
     <Content>
+      <Quotes quote={"Ok"} speaker={"Speaker"} />
       <NarutoImg
         src={narutoImg}
         alt="Naruto with Kunai"
