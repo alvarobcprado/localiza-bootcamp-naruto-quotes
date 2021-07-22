986b5dbc716c54516cbbfc5fad23ebcb20a00aba
Álvaro Prado <alvarob.prado@hotmail.com>
2021-07-22 11:38:36 -0400

    Adicionado State Hook ao componente App

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,21 +1,27 @@
 // import { ThemeProvider } from "styled-components";
 // import { Reset } from "styled-reset";
+import { useState } from "react";
 import narutoImg from "../../assets/images/naruto.png";
 import { Content, NarutoImg } from "./styles";
 import { Quotes } from "../../components";
-import { getQuotes } from "../../services";
+import { getQuote } from "../../services";
 
 function App() {
-  const onUpdate = () => {
-    console.log("test");
+  const [state, setState] = useState({
+    quote: "ok",
+    speaker: "Speaker",
+  });
+
+  console.log(state);
+
+  const onUpdate = async () => {
+    const quote = await getQuote();
+    console.log(quote);
+    setState(quote);
   };
   return (
     <Content>
-      <Quotes
-        quote={"Ok"}
-        speaker={"Speaker"}
-        onUpdate={onUpdate}
-      />
+      <Quotes {...state} onUpdate={onUpdate} />
       <NarutoImg
         src={narutoImg}
         alt="Naruto with Kunai"
