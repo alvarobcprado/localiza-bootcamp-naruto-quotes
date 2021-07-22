76ef3913383af6d6231cb01b5e37de7d66188d94
Álvaro Prado <alvarob.prado@hotmail.com>
2021-07-22 11:20:39 -0400

    Criado teste do QuotesServices dentro do teste App

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,11 +3,19 @@
 import narutoImg from "../../assets/images/naruto.png";
 import { Content, NarutoImg } from "./styles";
 import { Quotes } from "../../components";
+import { getQuotes } from "../../services";
 
 function App() {
+  const onUpdate = () => {
+    console.log("test");
+  };
   return (
     <Content>
-      <Quotes quote={"Ok"} speaker={"Speaker"} />
+      <Quotes
+        quote={"Ok"}
+        speaker={"Speaker"}
+        onUpdate={onUpdate}
+      />
       <NarutoImg
         src={narutoImg}
         alt="Naruto with Kunai"
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,30 @@
 import {
   render,
   screen,
+  fireEvent,
 } from "@testing-library/react";
 import { App } from "../components";
+
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+const response = {
+  speaker: "Speaker",
+  quote: "Teste quote",
+};
+
+const server = setupServer(
+  rest.get(
+    process.env.REACT_APP_API,
+    (req, res, ctx) => {
+      return res(ctx.json(response));
+    }
+  )
+);
+
+beforeAll(() => server.listen());
+beforeEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test("renders the app, with a button, a quote and a button", () => {
   render(<App />);
@@ -14,4 +36,16 @@ test("renders the app, with a button, a quote and a button", () => {
   expect(buttonEl).toBeInTheDocument;
   expect(imageEl).toBeInTheDocument;
   expect(textEl).toBeInTheDocument;
+});
+
+test("Chama a api ao clicar no botão e atualiza seu text", async () => {
+  render(<App />);
+  const buttonEl = screen.getByRole("button");
+  fireEvent.click(buttonEl);
+
+  const quoteEl = await screen.findByText(
+    response.quote
+  );
+
+  expect(quoteEl).toBeInTheDocument;
 });
