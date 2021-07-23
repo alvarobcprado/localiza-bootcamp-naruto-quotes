3620b5aaf6a1939fb77429a53b1920e031199538
Álvaro Prado <alvarob.prado@hotmail.com>
2021-07-23 12:24:53 -0400

    Alterado README.md

--- src/components/App/styles.jsx
+++ src/components/App/styles.jsx
@@ -2,14 +2,17 @@ import styled from "styled-components";
 
 export const Content = styled.div`
   height: 100vh;
+  box-sizing: border-box;
   padding: 0 58px;
   display: flex;
+  flex-wrap: wrap;
   flex-direction: row;
   justify-content: center;
   align-items: center;
 `;
 
 export const NarutoImg = styled.img`
-  max-width: 45vw;
+  max-width: 55vw;
+
   align-self: flex-end;
 `;
